5349367a4ada48d3d493835895016fccad98897d
Pedro Souza <pedrosport10@gmail.com>
2023-11-28 15:58:17 -0300

    Adicionando logica para os botões pagina 5

---
+++ src/tela05/index.js
@@ -0,0 +1,11 @@
+function irParaOutraPagina() {
+    window.location.replace('/src/tela06/pagina06.html');
+}
+
+function retornar(){
+    window.location.replace('/src/tela04/pagina04.html')
+}
+
+function skipPagina(){
+    window.location.replace('/src/tela06/pagina06.html');
+}
