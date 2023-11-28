39ad3234b92396aafeef5fa4ab53f30536920081
Pedro Souza <pedrosport10@gmail.com>
2023-11-28 14:11:38 -0300

    Adicionando JS para manipulação da pagina 03

---
+++ src/tela03/index.js
@@ -0,0 +1,16 @@
+//Botão de back volta, 
+//Botão de next disponivel quando os dados estiverem preenchidos (validação de input?)
+//botão de skip sempre disponivel, quando for fazer propagação dos dados, ele não vai passar nada, 
+//talvez uma string vazia
+
+function irParaOutraPagina() {
+    window.location.replace('/src/tela04/pagina04.html');
+}
+
+function retornar(){
+    window.location.replace('/src/tela02/pagina02.html')
+}
+
+function skipPagina(){
+    window.location.replace('/src/tela04/pagina04.html');
+}
