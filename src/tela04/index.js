78fd78536d19ef969c2a68373ffb4d2d16465adb
Pedro Souza <pedrosport10@gmail.com>
2023-11-28 15:47:02 -0300

    Adicionando JS para navegação da pagina 04

---
+++ src/tela04/index.js
@@ -0,0 +1,11 @@
+function irParaOutraPagina() {
+    window.location.replace('/src/tela05/pagina05.html');
+}
+
+function retornar(){
+    window.location.replace('/src/tela03/pagina03.html')
+}
+
+function skipPagina(){
+    window.location.replace('/src/tela05/pagina05.html');
+}
