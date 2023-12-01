87252f66c55b21f508db4cfa8808c6eca9e2f4e8
Pedro Souza <pedrosport10@gmail.com>
2023-12-01 02:22:01 -0300

    FEAT: Mudando texto hover da home

--- src/tela01/index.js
+++ src/tela01/index.js
@@ -1,3 +1,12 @@
 function irParaOutraPagina() {
     window.location.replace('/src/tela02/pagina02.html');
 }
+
+function mudarTextoHover(){
+    window.document.getElementById('btn').innerHTML = 'Just Go Ahead'
+}
+
+function restaurarTextoHover(){
+    window.document.getElementById('btn').innerHTML = `Let's go`
+
+}
