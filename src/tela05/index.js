283b2363678e4c67ffb8690cf506fb426ef05bc5
Pedro Souza <pedrosport10@gmail.com>
2023-12-01 17:38:30 -0300

    FIX: Ajustando responsividade da pagina 5

--- src/tela05/index.js
+++ src/tela05/index.js
@@ -9,3 +9,11 @@ function retornar(){
 function skipPagina(){
     window.location.replace('/src/tela06/pagina06.html');
 }
+
+function atualizarContador() {
+    var textarea = document.getElementById("detalhesPerca");
+    var contador = document.getElementById("textareaRestante");
+    var caracteresRestantes = 130 - textarea.value.length + ' caracteres restantes';
+
+    contador.innerHTML = caracteresRestantes;
+}
